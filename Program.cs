10634ff82a253233d8c6a086d62ebd5d5e7ca320
Lacey Walker <bluelarksong@gmail.com>
2020-08-31 14:38:57 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FortuneTeller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fortune = "Everything will be ok.";
+            Console.WriteLine(fortune);
+        }
+    }
+}
